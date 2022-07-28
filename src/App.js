c59f001df4c6b5ce3003d7bd84cca5d23d2ff998
allenaxie <allenaxie1@gmail.com>
2022-07-28 14:18:14 -0700

    Stub up login and profile components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Profile from './components/Profile';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <div className="App">
+        <Profile />
+        <Login />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <>
+            <div>Profile</div>
+            <p>Name: </p>
+            <p>Age:</p>
+            <p>Email: </p>
+        </>
+    )
+}
+
+export default Profile
